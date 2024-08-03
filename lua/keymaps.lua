5f6fc0147f0c72c25cd35ae8ba5b86cbd23e7717
Ryan <rataylor93@gmail.com>
2024-08-02 22:01:27 -0400

    added <leader>pv keymap and changed <CR> default in telescope

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,9 @@
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Open Netrw using <leader>pv
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open Netrw' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -29,6 +29,7 @@ return {
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>p', group = 'Navigate' },
       }
     end,
   },
